0cb6f983cfcd2cfa303071312a162be353e573a0
Oluwatosin Odubanjo <olutosinbanjo@gmail.com>
2022-05-11 11:49:51 +0100

    Update hello_world.cpp

--- exercises/hello_world.cpp
+++ exercises/hello_world.cpp
@@ -1,1 +1,15 @@
+#include <CL/sycl.hpp>
+#include <iostream>
 
+int main()
+{
+  // device selection
+  cl::sycl::queue queue_host{cl::sycl::host_selector{}};
+  cl::sycl::queue queue_gpu{cl::sycl::gpu_selector{}};
+
+  // print device information
+  std::cout << "First Try" ;
+  
+  return 0;
+  
+}
